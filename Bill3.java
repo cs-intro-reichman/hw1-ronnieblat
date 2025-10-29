dc15e686e9efbcc743d680d41fb743b2d19c949b
ronnie blat <ronnie.blat@post.runi.ac.il>
2025-10-29 22:47:02 +0200

    second upload

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,7 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    // Replace this comment with the rest of your code 
+		System.out.println(name1);  
 	}
 }
